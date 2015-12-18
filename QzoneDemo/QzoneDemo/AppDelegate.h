14ecf8cc200ff5aba008704de4f6ab32f15c98ff
dibadalu <1004205150@qq.com>
2015-12-18 20:38:58 +0800

    初始化demo

---
+++ QzoneDemo/QzoneDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QzoneDemo
+//
+//  Created by 陈泽嘉 on 15/12/18.
+//  Copyright (c) 2015年 dibadalu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QzoneDemo/QzoneDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QzoneDemo
+//
+//  Created by 陈泽嘉 on 15/12/18.
+//  Copyright (c) 2015年 dibadalu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
